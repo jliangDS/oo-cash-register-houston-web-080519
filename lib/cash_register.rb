584d655edc34ed75330658b7bfe330307d0a04f2
Jun Liang <illuminatemynight@gmail.com>
2019-08-10 20:07:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
-
+class CashRegister 
+  
+  attr_accessor :total 
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount 
+  end 
+  
+  def add_item(item, price, quantity = 1)
+    @price = price 
+    @quantity = quantity
+    @subtotal = @price * @quantity 
+    @total += @subtotal
+  end 
+  
+  def apply_discount
+    @discount = 200 
+    @total -= @discount 
+  end 
+  
+  def items 
+  end 
+  
+  def void_last_transaction 
+    
+  end 
+end 
